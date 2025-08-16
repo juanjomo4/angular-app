14555c3523e0c26c9af3ca351fe51007d8e14c4c
Juanjo M. <60217633+juanjomo4@users.noreply.github.com>
2025-08-16 10:29:51 +0200

    Conceptos básicos de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'Hola mundo Angular 19';
+  enabled = false;
+  courses = ['Angular', 'React', 'Vue', 'SpringBoot', 'Java', 'Python'];
+  setEnabled() {
+    this.enabled = this.enabled ? false : true;
+  }
 }
