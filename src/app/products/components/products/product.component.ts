5ca569671a068c6fb6f9ef4b33ae6bfaed9440d3
Juanjo M. <60217633+juanjomo4@users.noreply.github.com>
2025-08-20 11:27:17 +0200

    Segunda parte de edición de datos del product en form

--- src/app/products/components/products/product.component.ts
+++ src/app/products/components/products/product.component.ts
@@ -13,7 +13,7 @@ export class ProductComponent implements OnInit {
   products: Product[] = [];
 
   productSelected: Product = new Product();
-  
+
   constructor(private service: ProductService) {}
 
   /*
@@ -27,15 +27,32 @@ export class ProductComponent implements OnInit {
   }
 
   addProduct(product: Product) {
-    product.id = new Date().getTime(); // Asignar un ID único basado en la fecha actual
-    this.products.push(product); // Forma mutable
-    // Forma inmutable que se debe de usar en React,
-    // se esparcen los elementos del arreglo y creamos un nuevo arreglo para agregar el nuevo elemento.
-    // this.products = [...this.products, { ...product, id: new Date().getTime() }];
+    // Verifica si el producto ya tiene un ID asignado
+    // Si el ID es mayor a 0, significa que es un producto existente y se actualiza.
+    // Si el ID es 0 o negativo, significa que es un nuevo producto y se asigna un nuevo ID.
+    // En este caso, se asigna un ID único basado en la fecha actual.
+    if (product.id > 0) {
+      this.products = this.products.map((prod) => {
+        if (prod.id == product.id) {
+          return { ...product }; // Actualiza el producto existente
+        }
+        return prod; // Retorna el producto sin cambios
+      });
+    } else {
+      // Forma mutable
+      //product.id = new Date().getTime();
+      // this.products.push(product);
+
+      // Forma inmutable que se debe de usar en React,
+      // se esparcen los elementos del arreglo y creamos un nuevo arreglo para agregar el nuevo elemento.
+      this.products = [
+        ...this.products,
+        { ...product, id: new Date().getTime() },
+      ];
+    }
   }
 
   onUpdateProduct(product: Product) {
     this.productSelected = product;
-    
   }
 }
