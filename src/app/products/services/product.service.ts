0623f7754ea80ec97f0e4158503d2798daab6161
Juanjo M. <60217633+juanjomo4@users.noreply.github.com>
2025-08-29 11:42:38 +0200

    Consumiendo lista de productos desde el back  con springboot en local

--- src/app/products/services/product.service.ts
+++ src/app/products/services/product.service.ts
@@ -1,36 +1,46 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../models/Product';
-import { Observable, of } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  private products: Product[] = [
-    {
-      id: 1,
-      name: 'Sample Product',
-      description: 'This is a sample product description.',
-      price: 99.99,
-    },
-    {
-      id: 2,
-      name: 'Another Product',
-      description: 'This is another product description.',
-      price: 49.99,
-    },
-  ];
+  // private products: Product[] = [
+  //   {
+  //     id: 1,
+  //     name: 'Sample Product',
+  //     description: 'This is a sample product description.',
+  //     price: 99.99,
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Another Product',
+  //     description: 'This is another product description.',
+  //     price: 49.99,
+  //   },
+  // ];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+  private url = 'http://localhost:8080/products';
   /*
    * Método para obtener todos los productos en una lista y por id.
    * Se usa Observable para simular una llamada asíncrona.
    */
   findAll(): Observable<Product[]> {
-    return of(this.products);
+    // return of(this.products);
+
+    /*
+     * Llamada al backend
+     * Se usa map para transformar la respuesta del backend y extraer la lista de productos
+     */
+    return this.http
+      .get<Product[]>(this.url)
+      .pipe(map((response: any) => response._embedded.products as Product[]));
   }
 
-  findById(id: number): Observable<Product | undefined> {
-    return of(this.products.find((product) => product.id === id));
-  }
+  // findById(id: number): Observable<Product | undefined> {
+  //   return of(this.products.find((product) => product.id === id));
+  // }
 }
